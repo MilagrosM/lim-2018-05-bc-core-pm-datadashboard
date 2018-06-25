021cee412009655c8bb2eea844f740406c853790
MilagrosM <maytacasas21@gmail.com>
2018-06-25 12:21:18 -0500

    Funciones en data

--- src/data.js
+++ src/data.js
@@ -1,83 +1,204 @@
-//json
-const url1 = 'http://127.0.0.1:8080/data/cohorts.json'
-const url2 =  'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json'
-const url3 = 'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json'
-
-let selectCohorts = document.getElementById('select_cohorts');
-let filterButton = document.getElementById('filter_button');
-let usersList = document.getElementById('users_list');
-//let usersButton = document.getElementById('users_button')
-
-//Pintando cohorts
-fetch(url1)
- .then((response) => {
-   return response.json();
- })
- .then((cohorts) => {
-   let paintCohorts = '';
-   for (i = 0; i < cohorts.length; i++) {
-     paintCohorts += '<option value="' + i + '">' + cohorts[i].id + '</option>';
-   }
-   selectCohorts.innerHTML = paintCohorts;
- })
-
-
- //Pintando Nombres y Porcentajes
-filterButton.addEventListener("click", function () {
-  let value = selectCohorts.options[selectCohorts.selectedIndex].text
-  if(value === 'lim-2018-03-pre-core-pw') { 
-    fetch(url2)
-    .then((response) => {
-      return response.json();
-    })
-    .then((users) => {
-      let paintTable = ''
-      paintTable += '<tr>';
-      paintTable += '<th>Name</th>';
-      paintTable += '<th>%Completitud</th>';
-      paintTable += '<th>%Ejercicios</th>';
-      paintTable += '<th>%Lecturas</th>';
-      paintTable += '</tr>';
-      paintTable += '<tr>';
-      const filterRoles = users.filter(user => (user.role === "student"));
-        for (i = 0; i < filterRoles.length; i++) {
-          let roleStudents = filterRoles.map (filterRole => `${filterRole.name}`);
-          paintTable += '<td>' + roleStudents[i] + '</td>';
-          paintTable += '</tr>';
-        }
-      fetch(url3)
-      .then((response) => {
-        return response.json();
-      })
-      .then((progress) => {
-        for (i = 0; i < filterRoles.length; i++){
-          if (progress.hasOwnProperty(filterRoles[i].id)){
-            let abc = progress[filterRoles[i].id]; 
-            console.log (progress[filterRoles[i].id])
-            if (abc.hasOwnProperty('intro')) {
-              let abcd = abc.intro;
-              console.log(abc.intro)
-              if(abcd.hasOwnProperty('percent')){
-        
-                console.log(abcd.percent)
-    
-              }
+/*(users) => {
+;
       
-            }      
+    }
+  fetch(url3)
+  .then((response) => {
+    return response.json();
+  })
+  .then((progress) => {
+
+  }
+
+    
+    
+  })
+
+    //paintTable += '<td>' + exercises[i] + '</td>';
+    
+usersList.innerHTML = paintTable
+})   
+} 
+}*/
+
+/*movie = {
+  title: 'Back to the Future',
+  stars: ['Michael J. Fox', 'Christopher Lloyd', 'Lea Thompson'],
+};
+
+console.assert(movie.stars[0] === 'Michael J. Fox');
+console.assert(movie['stars'][1] === 'Christopher Lloyd');
+console.assert(movie.stars[2] === 'Lea Thompson'); */
+
+
+/*Crea la lista de usuarios a pintar y devuelve un array de usuarios 
+(llamado usersWithStats) que tiene la propiedad stats (que es un objeto)*/
+window.computeUsersStats = (users, progress/*, courses*/) => {
+  let usersWithStats = users.filter(user => (user.role === 'student'));
+  //Stats properties
+  let gPercent = 1;
+  let eTotal = 2;
+  let eCompleted = 3;
+  let ePercent = 4;
+  let rTotal = 5;
+  let rCompleted = 6;
+  let rPercent = 7;  
+  let qTotal = 8;
+  let qCompleted = 9;
+  let qPercent = 10;
+  let qScoreSum = 11;
+  let qScoreAvg = 12;
+  usersWithStats.forEach(object => {object.stats = {
+    percent: gPercent,
+    exercises: {
+      total: eTotal,
+      completed: eCompleted,
+      percent: ePercent,
+    },
+    reads: {
+      total: rTotal,
+      completed: rCompleted,
+      percent: rPercent,
+    },  
+    quizzes: {
+      total: qTotal,
+      completed: qCompleted,
+      percent: qPercent,
+      scoreSum: qScoreSum,
+      scoreAvg: qScoreAvg,
+    }
+  }})
+  console.log(usersWithStats)
+  let paintTable = ''
+  paintTable += '<tr>';
+  paintTable += '<th>Name</th>';
+  paintTable += '<th>%Completitud</th>';
+  paintTable += '<th>%Ejercicios</th>';
+  paintTable += '<th>%Lecturas</th>';
+  paintTable += '</tr>';
+  paintTable += '<tr>';   
+  for (i = 0; i < usersWithStats.length; i++) {
+    let usersName = usersWithStats.map (usersWithStat => `${usersWithStat.name}`);
+    console.log(usersWithStats[i].name)
+    paintTable += '<td>' + usersName[i] + '</td>'
+    paintTable += '</tr>';
+    /*if (progress.hasOwnProperty(usersWithStats[i].id)){
+      let abc = progress[usersWithStats[i].id]; 
+      console.log (progress[usersWithStats[i].id])
+      if (abc.hasOwnProperty('intro')) {
+        let abcd = abc.intro;
+        console.log(abc.intro)
+        if(abcd.hasOwnProperty('percent')){
+          let abcde = abcd.percent;
+          paintTable += '<td>' + abcde[i] + '</td>';
+          paintTable += '</tr>';
+  
+          console.log(abcd.percent)
+
+        }
+
+      }
+    
+
+    }*/      
+  }
+
+
+  usersList.innerHTML = paintTable
+  //return usersWithStats
+}
+
+  /*let userWithStats = [
+    {
+      name: 'Lizeth',
+      id: 'r5y35oiujr',
+      role: 'student',
+      timezone: 'America/Lima',
+      locale: 'es-ES',
+      signupCohort: 'lim-2018-03-pre-core-pw', // ya tengo esto
+      stats: {
+        percent: 95,
+        exercises: {
+          total: 5,
+          completed: 5,
+          percent: 100,
+        },
+        reads: {
+          total: 4,
+          completed: 2,
+          percent: 50,
+        },
+        quizzes: {
+          total: 2,
+          completed: 2,
+          percent: 100,
+          scoreSum: 190,
+          scoreAvg: 80,
         }
       }
+    }
+  ]*/
 
-        
-        
-      })
 
-        //paintTable += '<td>' + exercises[i] + '</td>';
-        
-    usersList.innerHTML = paintTable
-    })   
-  } else {
-  console.log("No hay data")}
-}) 
+
+window.processCohortData = (cohorts) => {
+  let paintCohorts = '';
+  for (i = 0; i < cohorts.length; i++) {
+    paintCohorts += '<option value="' + i + '">' + cohorts[i].id + '</option>';
+  }
+  selectCohorts.innerHTML = paintCohorts;
+}
+
+
+/*window.processCohortData = (options) => {
+return userWithStatsorderandfilter
+}
+
+
+Argumentos
+let options = {
+  cohort: {},
+  cohortData: {
+    users: [],
+    progress: {},
+  }
+  orderBy: "",
+  orderDirection: "",
+  search: "",
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let usersButton = document.getElementById('users_button')
+ */
 
 
   
--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-var loginButton = document.getElementById('login_button');
-var studentsButton = document.getElementById('students_button');
+//Views
+let view1 = document.getElementById('view_1');
+let view2 = document.getElementById('view_2');
+let view3 = document.getElementById('view_3');
+let header = document.getElementById('header');
 
-var view1 = document.getElementById('view_1');
-var view2 = document.getElementById('view_2');
-var view3 = document.getElementById('view_3');
-var header = document.getElementById('header');
+//Links JSON
+const url1 = 'http://127.0.0.1:8080/data/cohorts.json'
+const url2 =  'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/users.json'
+const url3 = 'http://127.0.0.1:8080/data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+const urls = [url1, url2, url3];
 
+//Buttons
+let loginButton = document.getElementById('login_button');
+let studentsButton = document.getElementById('students_button');
+let filterButton = document.getElementById('filter_button');
+
+let selectCohorts = document.getElementById('select_cohorts');
+let usersList = document.getElementById('users_list');
+
+//Events
 loginButton.addEventListener('click',function(){
   view1.style.display = "none";
   view2.style.display = "block";
@@ -18,7 +31,55 @@ studentsButton.addEventListener('click',function(){
   view2.style.display = "none";
   header.style.display = "block";
   view3.style.display = "block";
+  fetch(url1)
+  .then((response) => {
+    return response.json();
+  })
+  .then((cohorts) => { 
+    processCohortData(cohorts)
+  })
 });
+
+filterButton.addEventListener("click", function () {
+  let value = selectCohorts.options[selectCohorts.selectedIndex].text
+  if (value === 'lim-2018-03-pre-core-pw') { 
+    fetch(url2)
+    .then((response) => {
+      return response.json();
+    })
+    .then((users) => { 
+      computeUsersStats(users, progress)
+    })
+
+    const fetchRequest = [];
+    fetchRequest.push(fetch(url2));
+    fetchRequest.push(fetch(url3));
+    Promise.all(fetchRequest).then((responses) => {
+      let prueba = responses.map(response => response.json());
+      console.log(prueba)
+      return prueba
+    })
+    .then((users=url2.users[id], progress) =>{
+      computeUsersStats(users, progress)
+    })
+  } else {
+    console.log("No hay data")
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
